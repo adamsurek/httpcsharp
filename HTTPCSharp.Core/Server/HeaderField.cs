2caa0b622c0a447061a1c1a7644ec0851b154bb4
Adam Surek <26888201+SuRektM8@users.noreply.github.com>
2024-05-18 10:56:40 -0400

    Added resources for testing requests

---
+++ HTTPCSharp.Core/Server/HeaderField.cs
@@ -0,0 +1,33 @@
+﻿namespace HTTPCSharp.Core.Requests;
+
+
+
+private Dictionary<HeaderFieldEnum, string> _headerFieldTranslation = new()
+{
+	{ HeaderFieldEnum.Accept, "Accept" },
+	{ HeaderFieldEnum.AcceptCharset, "Accept-Charset" },
+	{ HeaderFieldEnum., "Accept" },
+	{ HeaderFieldEnum.Accept, "Accept" },
+	{ HeaderFieldEnum.Accept, "Accept" },
+	{ HeaderFieldEnum.Accept, "Accept" },
+};
+public enum HeaderFieldEnum
+{
+	Accept,
+	AcceptCharset,
+	AcceptEncoding,
+	AcceptLanguage,
+	Authorization,
+	ContentEncoding,
+	ContentLength,
+	ContentType,
+	Date,
+	Host,
+	IfMatch,
+	IfModifiedSince,
+	IfRange,
+	IfUnmodifiedSince,
+	Range,
+	Server,
+	UserAgent
+}
---
+++ Resources/site/home/home.html.js
@@ -0,0 +1,12 @@
+﻿function toggleColorMode() {
+    var body = document.getElementById("body");
+    
+    
+    if (body.style.backgroundColor === "white") {
+        body.style.backgroundColor = "#262626";
+        body.style.color = "white"
+    } else {
+        body.style.backgroundColor = "white";
+        body.style.color = "black"
+    }
+}
