f6104fe3b99a8c789722829fcb06e56462065792
Adam Surek <26888201+SuRektM8@users.noreply.github.com>
2024-05-19 14:27:14 -0400

    Basic GET response - images are broken

--- HTTPCSharp.Core/Server/RequestEvaluator.cs
+++ HTTPCSharp.Core/Server/RequestEvaluator.cs
@@ -1,4 +1,6 @@
-﻿using System.Xml;
+﻿using System.Net.Mime;
+using System.Text;
+using System.Xml;
 using HTTPCSharp.Core.Requests;
 using HTTPCSharp.Core.Responses;
 
@@ -25,6 +27,10 @@ public static class RequestEvaluator
 		{
 			case RequestMethodEnum.Options:
 				response = HandleOptionsRequest(request.RequestLine.RequestUri);
+				break;
+			
+			case RequestMethodEnum.Get:
+				response = HandleGetRequest(request);
 				break;
 			
 			default:
@@ -75,6 +81,87 @@ public static class RequestEvaluator
 
 		return null;
 	}
+	
+	private static InternalResource GetResourceInformation(string path)
+	{
+		List<RequestMethodEnum> allowedMethods = new();
+		
+		XmlDocument sitePermissions = new();
+		sitePermissions.Load(Path.Combine(ResourcesPath, "SitePermissions.xml"));
+
+		XmlNode? resourceNode = sitePermissions.DocumentElement!.SelectSingleNode($"//HttpResource[@name='{path}']");
+
+		if (resourceNode is null)
+		{
+			throw new Exception($"SITE PERMISSIONS XML IS MISSING RESOURCE NODE '{path}'");
+		}
+
+		if (resourceNode.Attributes?["type"]?.InnerText is null)
+		{
+			throw new Exception($"RESOURCE NODE IS MISSING TYPE ATTRIBUTE: '{resourceNode.Name}'");
+		}
+
+		string resourceTypeAttribute = resourceNode.Attributes!["type"]!.InnerText;
+
+		bool isValidResourceType = ResourceTypeEnum.TryParse(resourceTypeAttribute, true, out ResourceTypeEnum resourceType);
+
+		if (!isValidResourceType)
+		{
+			throw new Exception($"INVALID RESOURCE TYPE: '{resourceTypeAttribute}'");
+		}
+
+		XmlNode? filePropertiesNode = resourceNode.SelectSingleNode("FileProperties");
+
+		if (filePropertiesNode is null)
+		{
+			throw new Exception($"FILE RESOURCE IS MISSING FILE PROPERTIES NODE: '{resourceNode.Name}'");
+		}
+			
+		XmlNode? fileMimeTypeNode = filePropertiesNode.SelectSingleNode("MIMEType");
+		if (fileMimeTypeNode is null)
+		{
+			throw new Exception($"FILE RESOURCE IS MISSING MIME TYPE NODE: '{filePropertiesNode.Name}'");
+		}
+
+		// string mimeTypeText = fileMimeTypeNode.InnerText;
+		MimeType resourceMimeType = new(fileMimeTypeNode.InnerText);
+
+		// bool isValidMimeType = MimeTypeEnum.TryParse(mimeTypeText, true, out resourceMimeType);
+		// if (!isValidMimeType)
+		// {
+		// 	throw new Exception($"INVALID RESOURCE TYPE: '{resourceTypeAttribute}'");
+		// }
+
+		XmlNodeList? methodNodes = resourceNode.SelectSingleNode("AllowedRequestMethods")!.SelectNodes("Method");
+
+		if (methodNodes is null)
+		{
+			allowedMethods.Add(RequestMethodEnum.Options);
+		}
+		else
+		{
+			foreach (XmlNode methodNode in methodNodes)
+			{
+				if (methodNode.InnerText is null)
+				{
+					throw new Exception($"METHOD NODE IS MISSING VALUE: '{methodNode.Value}'");
+				}
+
+				string methodNodeValue = methodNode.InnerText;
+
+				bool isValidRequestMethod = RequestMethodEnum.TryParse(methodNodeValue, ignoreCase: true, out RequestMethodEnum method);
+			
+				if (!isValidRequestMethod)
+				{
+					throw new Exception($"UNHANDLED REQUEST METHOD: '{method}'");
+				}
+			
+				allowedMethods.Add(method);
+			}
+		}
+
+		return new InternalResource(Path.Combine(ResourcesPath, path), resourceType, resourceMimeType, allowedMethods);
+	}
 
 	private static List<RequestMethodEnum> CheckAllowedRequestMethods(string path)
 	{
@@ -123,4 +210,44 @@ public static class RequestEvaluator
 
 		return allowedMethods;
 	}
+
+	private static Response HandleGetRequest(Request request)
+	{
+		List<ResponseHeader> headers = new()
+		{
+			new(HeaderFieldTypeEnum.Date, DateTime.Now.ToUniversalTime().ToString("R")),
+			new(HeaderFieldTypeEnum.Server, "nunya")
+		};
+		string? resourcePath = GetResourcePath(request.RequestLine.RequestUri.ToFilePath());
+
+		if (resourcePath != null)
+		{
+			// List<RequestMethodEnum> allowedMethods = CheckAllowedRequestMethods(request.RequestLine.RequestUri.Path);
+			InternalResource resource = GetResourceInformation(request.RequestLine.RequestUri.Path);
+
+			if (!resource.AllowedRequestMethods.Contains(RequestMethodEnum.Get))
+			{
+				headers.Add(new ResponseHeader(HeaderFieldTypeEnum.Allow, string.Join(", ", resource.AllowedRequestMethods).ToUpper()));
+				return new Response(new StatusLine(StatusCodesEnum.MethodNotAllowed, "Method Not Allowed"), headers,
+					$"GET request made to '{request.RequestLine.RequestUri.Path}' - HTTP METHOD NOT ALLOWED");
+			}
+
+			string body;
+			if (resource.MimeType.Name.StartsWith("image"))
+			{
+				body = Encoding.ASCII.GetString(File.ReadAllBytes(resourcePath));
+			}
+			else
+			{
+				 body = File.ReadAllText(resourcePath);
+			}
+			headers.Add(new ResponseHeader(HeaderFieldTypeEnum.ContentLength, body.Length.ToString()));
+			headers.Add(new ResponseHeader(HeaderFieldTypeEnum.AcceptRanges, "bytes"));
+			headers.Add(new ResponseHeader(HeaderFieldTypeEnum.ContentType, resource.MimeType.Name));
+			return new Response(new StatusLine(StatusCodesEnum.Ok, "OK"), headers, body);
+		}
+		
+		return new Response(new StatusLine(StatusCodesEnum.NotFound, "Not Found"), headers, $"OPTIONS request made to '{request.RequestLine.RequestUri.Path}' - RESOURCE NOT FOUND");
+	}
+
 }
